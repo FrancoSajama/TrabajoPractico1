5eff2b92875aeac62a402f8f277310c1c4b708aa
usuario <usuario@192.168.0.14>
2024-04-09 04:40:42 -0300

    Se añade ejercicio 2

---
+++ tp1_MamaníSajamaFrancoNahuel/src/ar/edu/unju/fi/ejercicio2/Main.java
@@ -0,0 +1,19 @@
+package ar.edu.unju.fi.ejercicio2;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String nombrePersona="Sergio", telefono="3885432786";
+		int edadPersona=54;
+		float alturaEdificio=100.4f, precioProducto=499.99f;
+		double calculoCoseno=Math.cos(0.5) ;
+		
+		System.out.println("Nombre de persona: "+nombrePersona+".");
+		System.out.println("Edad de persona: "+edadPersona+" años.");
+		System.out.println("Altura de un edificio: "+alturaEdificio+" m.");
+		System.out.println("Precio producto de supermercado: "+precioProducto+" $.");
+		System.out.println("Calculo de coseno de 0.5: "+calculoCoseno+".");
+	}
+
+}
