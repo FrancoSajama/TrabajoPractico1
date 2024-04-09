f2b55435477f522c1d88da6498a9c7cc3c1fddf5
usuario <usuario@192.168.0.14>
2024-04-09 20:30:09 -0300

    Ejercicio 4 resuelto

---
+++ tp1_MamaníSajamaFrancoNahuel/src/ar/edu/unju/fi/ejercicio4/Main.java
@@ -0,0 +1,29 @@
+package ar.edu.unju.fi.ejercicio4;
+
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		int num, aux, factorial=1;
+		Scanner sc=new Scanner(System.in);
+		
+		System.out.print("Ingrese un numero del 1 al 10: ");
+		num=sc.nextInt();
+		
+		if(num>=0 && num<=10) {
+			if(num!=0) {
+				aux=num;
+				while(aux!=0) {
+					factorial*=aux;
+					aux--;
+				}
+			}
+			System.out.println("El factorial de "+num+" es: "+factorial);
+		}else 
+			System.out.print("El numero ingresado no esta en el rango especificado.");
+			
+		
+	}
+
+}
