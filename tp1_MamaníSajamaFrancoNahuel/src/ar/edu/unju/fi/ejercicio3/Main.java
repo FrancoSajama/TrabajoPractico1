0cd1a31f838d17072400ec5a72d3ce8456026778
usuario <usuario@192.168.0.14>
2024-04-09 19:11:23 -0300

    Ejercicio 3 resuelto

---
+++ tp1_MamaníSajamaFrancoNahuel/src/ar/edu/unju/fi/ejercicio3/Main.java
@@ -0,0 +1,22 @@
+package ar.edu.unju.fi.ejercicio3;
+
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		Scanner sc=new Scanner(System.in);
+		int numero;
+		
+		System.out.print("Ingrese un numero: ");
+		numero=sc.nextInt();
+		
+		if(numero%2==0) {
+			System.out.println("El numero "+numero+" es par."+" El nuevo numero es: "+numero*3);
+		}else {
+			System.out.println("El numero "+numero+" es impar."+" El nuevo numero es: "+numero*2);
+		}
+	}
+
+}
