f078d345fa85ace67c466363cdb88aec9a258f3d
usuario <usuario@192.168.0.14>
2024-04-09 03:48:15 -0300

    Ejercicio 1 realizado.

--- tp1_MamaníSajamaFrancoNahuel/src/ar/edu/unju/fi/ejercicio1/Main.java
+++ tp1_MamaníSajamaFrancoNahuel/src/ar/edu/unju/fi/ejercicio1/Main.java
@@ -4,7 +4,10 @@ public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		int notaMatematica=6, notaQuimica=7, notaFisica=9, notaLiteratura=5, notaHistoria=4, sumaNotas=0;
+		float promedioNotas=(float)(notaMatematica+notaQuimica+notaFisica+notaLiteratura+notaHistoria)/5;
+		
+		System.out.println("El promedio de notas es: "+promedioNotas);
 	}
 
 }
